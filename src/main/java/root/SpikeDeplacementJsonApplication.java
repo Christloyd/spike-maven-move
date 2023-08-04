e6043b26835184e731d4eb9f1584c0a91c124d4d
CestMoiChrist <svay.christloyd@gmail.com>
2023-08-04 14:34:05 +0200

    premier commit

---
+++ src/main/java/root/SpikeDeplacementJsonApplication.java
@@ -0,0 +1,13 @@
+package root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeDeplacementJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpikeDeplacementJsonApplication.class, args);
+	}
+
+}
